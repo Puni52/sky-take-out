1b32f0c9e921cc80ca176a7e0b7ef451cf445f92
52Hertz <1715885085@qq.com>
2024-02-29 17:17:00 +0800

    公共字段自动填充 测试

---
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -0,0 +1,33 @@
+package com.sky.aspect;
+
+/*
+*
+* 自定义切面
+* */
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class AutoFillAspect {
+    /*
+    *
+    * 切入点
+    * */
+    @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
+    public void autoFillPointCut(){}
+
+    @Before("autoFillPointCut()")
+    public void autoFill(JoinPoint joinPoint){
+        log.info("开始进行公共字段自动填充");
+
+
+    }
+}
