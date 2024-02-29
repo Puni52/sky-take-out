17e96b3bafcf75ebe6b3b8e295d3366ef50007e8
52Hertz <1715885085@qq.com>
2024-02-29 14:20:59 +0800

    启用/禁用员工账号

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -107,4 +107,26 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
 
+
+
+    /*
+     *
+     * 启用禁用员工账号
+     * */
+
+    public void startOrStop(Integer status, Long id){
+    // update employee set status=? where id =?
+
+        Employee employee=new Employee();
+        employee.setStatus(status);
+        employee.setId(id);
+
+        /*Employee employee =Employee.builder()
+                .status(status)
+                .id(id)
+                .build();*/
+
+        employeeMapper.update(employee);
+
+    }
 }
