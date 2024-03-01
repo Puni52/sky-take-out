3e2684590bb83d6ff60e8bcf1bcbd610c23e9f89
52Hertz <1715885085@qq.com>
2024-03-01 23:16:32 +0800

    修改套餐

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -4,11 +4,14 @@ import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
 import com.sky.enumeration.OperationType;
 import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface SetmealMapper {
@@ -44,4 +47,6 @@ public interface SetmealMapper {
     void deleteById(Long setmealId);
 
 
+    @AutoFill(value = OperationType.UPDATE)
+    void update(Setmeal setmeal);
 }
