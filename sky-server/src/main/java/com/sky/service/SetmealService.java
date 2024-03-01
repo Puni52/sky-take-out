590f4efcc05608c3ba16871230e02a63f5e83d09
52Hertz <1715885085@qq.com>
2024-03-01 23:03:27 +0800

    套餐分页查询

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface SetmealService {
 
@@ -13,4 +15,13 @@ public interface SetmealService {
     * 新增套餐
     * */
     void save(SetmealDTO setmealDTO);
+
+
+
+    /*
+    *
+    *
+    * 套餐分页查询
+    * */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
