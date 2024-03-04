a1c79d3b542a5bd07f4cccc6ef94d5b10c88c255
52Hertz <1715885085@qq.com>
2024-03-04 16:09:48 +0800

    查看购物车

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,6 +1,9 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
 
 public interface ShoppingCartService {
 
@@ -12,4 +15,13 @@ public interface ShoppingCartService {
     * 添加购物车
     * */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+
+
+    /*
+    *
+    * 查看购物车
+    *
+    * */
+    List<ShoppingCart> showShoppingCart();
 }
