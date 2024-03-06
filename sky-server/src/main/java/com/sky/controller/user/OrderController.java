586d868a156032daaf0fde1f4cdf7bb303bbad43
52Hertz <1715885085@qq.com>
2024-03-06 14:49:26 +0800

    用户端订单模块 管理端订单模块

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -1,11 +1,14 @@
 package com.sky.controller.user;
 
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.dto.OrdersPaymentDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -52,6 +55,65 @@ public class OrderController {
         log.info("生成预支付交易单：{}", orderPaymentVO);
         return Result.success(orderPaymentVO);
     }
+
+
+    /*
+    *
+    *
+    * 历史订单分页查询
+    * */
+    @GetMapping("/historyOrders")
+    public Result<PageResult> history(int page,int pageSize,Integer status ){
+        log.info("历史订单分页查询，{},{},{}",page,pageSize,status);
+        PageResult pageResult= orderService.history(page,pageSize,status);
+
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 查询订单详情
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/orderDetail/{id}")
+    @ApiOperation("查询订单详情")
+    public Result<OrderVO> details(@PathVariable("id") Long id) {
+        OrderVO orderVO = orderService.details(id);
+        return Result.success(orderVO);
+    }
+
+
+    /**
+     * 用户取消订单
+     *
+     * @return
+     */
+    @PutMapping("/cancel/{id}")
+    @ApiOperation("取消订单")
+    public Result cancel(@PathVariable("id") Long id) throws Exception {
+        orderService.userCancelById(id);
+        return Result.success();
+    }
+
+    /**
+     * 再来一单
+     *
+     * @param id
+     * @return
+     */
+    @PostMapping("/repetition/{id}")
+    @ApiOperation("再来一单")
+    public Result repetition(@PathVariable Long id) {
+        orderService.repetition(id);
+        return Result.success();
+    }
+
+
+
+
+
+
 
 
 
