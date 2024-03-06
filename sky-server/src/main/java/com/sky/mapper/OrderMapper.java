8e20082980e23a67c232ea54a78e760435569ae3
52Hertz <1715885085@qq.com>
2024-03-06 16:39:25 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -7,6 +7,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
 
@@ -58,6 +61,9 @@ public interface OrderMapper {
 
 
 
+//    根据订单状态和下单时间查询订单
+    @Select("select * from orders where status=#{status} and order_time<#{orderTime}")
+    List<Orders>getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 
 
 
